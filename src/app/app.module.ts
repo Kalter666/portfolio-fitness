16ff6d7f6cfd27a511d9b48551991f0da2a865bc
Kalter <kalterxad34@gmail.com>
2018-04-22 21:11:38 +0300

    generated comp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,25 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { MainComponent } from './main/main.component';
+import { AboutComponent } from './about/about.component';
+import { DirectionsComponent } from './directions/directions.component';
+import { PhotosComponent } from './photos/photos.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    MainComponent,
+    AboutComponent,
+    DirectionsComponent,
+    PhotosComponent,
+    ContactsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
