e0bd15dde513c66b9820ef67f240e39f23cc0bb2
Kalter <kalterxad34@gmail.com>
2018-08-02 20:07:03 +0300

    finale

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { APP_ID, Inject, NgModule, PLATFORM_ID } from '@angular/core';
+import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
@@ -52,11 +52,4 @@ import { isPlatformBrowser } from '@angular/common';
   ]
 })
 export class AppModule {
-  constructor(
-    @Inject(PLATFORM_ID) private platformId: Object,
-    @Inject(APP_ID) private appId: string) {
-    const platform = isPlatformBrowser(platformId) ?
-      'in the browser' : 'on the server';
-    console.log(`Running ${platform} with appId=${appId}`);
-  }
 }
